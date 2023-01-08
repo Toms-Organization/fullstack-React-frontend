4f7825d079379ed78aec52482929f62e2ff67dbe
TomIsLearningJava <tom.ganemo1987@gmail.com>
2023-01-08 15:03:34 +0100

    frame is okay, need to fill with content

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 
 import NavBar from './components/NavBar';
 import HomePage from './components/HomePage';
-import Profile from './components/Profile';
+import AboutPage from './components/AboutPage';
 import LoginPage from './components/LoginPage';
+import NewsPage from './components/NewsPage';
 
 import { Route, Routes } from 'react-router-dom';
-import AboutPage from './components/AboutPage';
 
 function App() {
   return (
@@ -17,6 +17,7 @@ function App() {
         <Routes>
           <Route path="/Home" element={<HomePage />} />
           <Route path="/About" element={<AboutPage />} />
+          <Route path="/News" element={<NewsPage />} />
           <Route path="/Login" element={<LoginPage />} />
         </Routes>
       </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,22 +5,26 @@ const NavBar = () => {
   return (
     <nav className="flex justify-between items-center w-full h-40 px-5 py-5  text-white bg-black">
       <Link to="/Home" className="homePage">
-        <h1 className="flex text-5xl font-signature ml-2 justify-start">
+        <h1 className="flex text-7xl font-signature ml-2 justify-start">
           Tom Ganemo
         </h1>
       </Link>
-      <h3 className="navbar-links">
-        <h3>
-          <Link to="/Login" className="LoginPage">
-            Login/Logout
-          </Link>
-        </h3>
-        <h3>
-          <Link to="/About" className="AboutPage">
-            About
-          </Link>
-        </h3>
-      </h3>
+
+      <Link to="/Home" className="HomePage">
+        Home
+      </Link>
+
+      <Link to="/About" className="AboutPage">
+        About
+      </Link>
+
+      <Link to="/News" className="NewsPage">
+        News
+      </Link>
+
+      <Link to="/Login" className="LoginPage">
+        Login/Logout
+      </Link>
     </nav>
   );
 };
