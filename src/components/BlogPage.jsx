56cf0e106bec0bf28121af2adc71d230f1ed63e2
TomIsLearningJava <tom.ganemo1987@gmail.com>
2023-01-18 19:39:25 +0100

    added BlogServie just to test an endpoint. Struggled a lot with CORS and the security aspect. Next step is to implement functions/services to fetch and render BlogPosts with auth headers

--- src/components/BlogPage.jsx
+++ src/components/BlogPage.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import axios from 'axios';
+import { getData } from '../services/blogService';
+import { useState, useEffect } from 'react';
 
 const BlogPage = () => {
+  const [ActiveString, setActiveString] = useState();
+
+  useEffect(() => {
+    const getDataConst = async () => {
+      const temp = await getData();
+      setActiveString(temp);
+    };
+    getDataConst();
+  }, []);
+
+  //const myString = getData();
+  //console.log(myString);
+
   return (
     <div>
       <h1>BlogPage</h1>
+      <p>{ActiveString}</p>
     </div>
   );
 };
