511e3f0bc2a11164a5cba60164cf7c1543638ab8
TomIsLearningJava <tom.ganemo1987@gmail.com>
2023-01-19 13:20:03 +0100

    .

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <NavBar />
       <div>
         <Routes>
-          <Route path="/Home" element={<HomePage />} />
+          <Route path="" element={<HomePage />} />
           <Route path="/About" element={<AboutPage />} />
           <Route path="/News" element={<NewsPage />} />
           <Route path="/Blog" element={<BlogPage />} />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom';
 const NavBar = () => {
   return (
     <nav className="flex justify-between items-center text-2xl w-full h-40 px-5 py-5 font-plainheader text-white bg-black">
-      <Link to="/Home" className="homePage">
+      <Link to="" className="homePage">
         <h1 className="flex text-8xl font-signature ml-2 justify-start">
           Tom Ganemo
         </h1>
       </Link>
 
-      <Link to="/Home" className="HomePage">
+      <Link to="" className="HomePage">
         Home
       </Link>
 
