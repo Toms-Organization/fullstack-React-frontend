b7d978ed2deea871ebbe62deee36d9b6f1079df9
TomIsLearningJava <tom.ganemo1987@gmail.com>
2023-01-19 15:45:55 +0100

    Added a CardTestComponent to display all Blog entries, looks okay now

---
+++ src/components/CardTest.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const CardTest = ({ allBlogPosts }) => {
+  return (
+    <div className="px-5 py-5  text-2xl font-plainheader bg-black">
+      <div class="rounded-lg shadow-lg flex justify-center p-6 mx-60 text-black bg-black px-2 py-2">
+        <div class="p-6">
+          {allBlogPosts.map((blogpost, index) => (
+            <div
+              key={blogpost.id}
+              className="bg-zinc-400 rounded-lg shadow-lg my-2 "
+            >
+              <br />
+              <h3 class="flex justify-center font-plaintext text-4xl font-bold mb-8">
+                {blogpost.topic}
+              </h3>
+              <p class=" text-gray-900 font-plaintext text-base mb-8 italic mx-7">
+                {blogpost.text}
+              </p>
+              <div class="flex ">
+                <h5 class="text-gray-900 text-base font-medium mb-2 mx-7">
+                  Written by: {blogpost.byUser}
+                </h5>
+                <h5 class="text-gray-900 text-base font-medium mb-2 mx-7">
+                  Date: {blogpost.createdAt}
+                </h5>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardTest;
--- src/pages/NewsPage.jsx
+++ src/pages/NewsPage.jsx
@@ -3,9 +3,10 @@ import { getData, getAllBlogPosts } from '../services/blogService';
 import { useState, useEffect } from 'react';
 import BlogPost from '../components/BlogPost';
 import FormLoginTest from '../components/FormLoginTest';
+import CardTest from '../components/CardTest';
 
 const NewsPage = () => {
-  /*   const [blogPosts, setblogPosts] = useState();
+  const [blogPosts, setblogPosts] = useState();
 
   useEffect(() => {
     const getDataConst = async () => {
@@ -15,13 +16,17 @@ const NewsPage = () => {
     getDataConst();
   }, []);
 
-  console.log(blogPosts); */
+  console.log(blogPosts);
 
   return (
-    <div>
-      <FormLoginTest></FormLoginTest>
+    <div className="min-h-screen font-plainheader text-white bg-black">
+      <h1 className=" flex justify-center text-8xl font-signature p-8">
+        This is the News Page!
+      </h1>
     </div>
   );
 };
 
 export default NewsPage;
+
+// <FormLoginTest></FormLoginTest>;
