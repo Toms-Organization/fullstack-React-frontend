6f74069f22f111e44df2edb8c2fb4df943505d48
TomIsLearningJava <tom.ganemo1987@gmail.com>
2023-01-19 14:30:09 +0100

    addedFormloginFromtailwind

--- src/pages/NewsPage.jsx
+++ src/pages/NewsPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { getData, getAllBlogPosts } from '../services/blogService';
 import { useState, useEffect } from 'react';
 import BlogPost from '../components/BlogPost';
+import FormLoginTest from '../components/FormLoginTest';
 
 const NewsPage = () => {
   /*   const [blogPosts, setblogPosts] = useState();
@@ -17,13 +18,8 @@ const NewsPage = () => {
   console.log(blogPosts); */
 
   return (
-    <div className=" min-h-screen px-5 py-5 text-2xl underline font-plainheader text-white bg-black">
-      <h1>NewsPage</h1>
-      <h3>
-        This page will interact with another API getting news in the world
-        fitting for me
-      </h3>
-      {/*  {blogPosts && <BlogPost allBlogPosts={blogPosts} />} */}
+    <div>
+      <FormLoginTest></FormLoginTest>
     </div>
   );
 };
